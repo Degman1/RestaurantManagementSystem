21e31feb9963010d5da8910cd0d858fe95af4c70
David Gerard <davidgerard@1x-nat-vl932-172-30-204-227.wireless.umass.edu>
2022-01-25 15:36:17 -0500

    finish open/close and menu import testing

--- tests/RestaurantManagementSystem.test.js
+++ tests/RestaurantManagementSystem.test.js
@@ -4,52 +4,67 @@ test("create new obj", () => {
   const rms = new RMS("1234");
 });
 
+// Test open and closing
+
 test("open a closed restaurant correct password", () => {
   const rms = new RMS("1234");
-  rms.open("1234").then(x => { expect(x).toBeTruthy(); });
-  expect(rms.isOpen).toBe(true);
+  rms.openShop("1234").catch(err => {});
+  expect(rms.isOpen()).toBeTruthy();
 });
 
 test("open a closed restaurant incorrect password", () => {
   const rms = new RMS("1234");
-  rms.open("4321").catch(err => {});
+  rms.openShop("4321").catch(err => {});
   expect(rms.isOpen()).toBeFalsy();
 });
 
 test("open an open restaurant correct password", () => {
   const rms = new RMS("1234");
-  rms.open("1234").catch(err => {});
-  rms.open("1234").catch(err => {});
+  rms.openShop("1234").catch(err => {});
+  rms.openShop("1234").catch(err => {});
   expect(rms.isOpen()).toBeTruthy();
 });
 
 test("close an open restaurant correct password", () => {
   const rms = new RMS("1234");
-  rms.open("1234").catch(err => {});
-  rms.close("1234").catch(err => {});
+  rms.openShop("1234").catch(err => {});
+  rms.closeShop("1234").catch(err => {});
   expect(rms.isOpen()).toBeFalsy();
 });
 
 test("close an open restaurant incorrect password", () => {
   const rms = new RMS("1234");
-  rms.open("1234").catch(err => {});
-  rms.close("4321").catch(err => {});
+  rms.openShop("1234").catch(err => {});
+  rms.closeShop("4321").catch(err => {});
   expect(rms.isOpen()).toBeTruthy();
 });
 
 test("close a closed restaurant correct password", () => {
   const rms = new RMS("1234");
-  rms.close("1234").catch(err => {});
+  rms.closeShop("1234").catch(err => {});
   expect(rms.isOpen()).toBeFalsy();
 });
-// TODO: everything past here
+
 test("closing a restaurant closes the timesheet", () => {
   const rms = new RMS("1234");
-  rms.open("1234").catch(err => {});
-  rms.close("1234").catch(err => {});
+  rms.openShop("1234").catch(err => {});
+  rms.closeShop("1234").catch(err => {});
   expect(rms.timesheet.isOpen()).toBeFalsy();
 });
 
-test("closing a restaurant clears the host waitlist", () => {
+test("closing a restaurant clears the host waitlist", () => {   // TODO once create host capabilities
+  const rms = new RMS("1234");
+  rms.openShop("1234").catch(err => {});
+  // create a host here and check the length to be 1
+  rms.closeShop("1234").catch(err => {});
+  expect(rms.host.waitlist.length).toBeFalsy();
+});
 
+// test importing the menu and adding food items
+
+test("import json menu data into system", () => {
+  const jsonData = require('../data/MenuExample.json');
+  const rms = new RMS("1234");
+  rms.importMenu(jsonData);
+  expect(jsonData).toEqual(rms.menu.items);
 });
